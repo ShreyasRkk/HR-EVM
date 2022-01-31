4fb7e1d97fb82c1bf04e01852433bd7ed668d01d
Shreyas R.K <74357270+ShreyasRkk@users.noreply.github.com>
2022-01-31 20:20:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+import cv2
+import pyramids
+import heartrate
+import preprocessing
+import eulerian
+import time
+c=0
+# Frequency range for Fast-Fourier Transform
+freq_min =0.7
+freq_max =2
+
+t0 = time.time()
+
+video = cv2.VideoCapture(0)
+# We need to check if camera
+# is opened previously or not
+if (video.isOpened() == False):
+    print("Error reading video file")
+
+# We need to set resolutions.
+# so, convert them from float to integer.
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+
+size = (frame_width, frame_height)
+
+# Below VideoWriter object will create
+# a frame of above defined The output
+# is stored in 'filename.avi' file.
+result = cv2.VideoWriter('filename.mov',
+                         cv2.VideoWriter_fourcc(*'MJPG'),
+                         6, size)
+
+while (True):
+    ret, frame = video.read()
+
+    if ret == True:
+
+        # Write the frame into the
+        # file 'filename.avi'
+        result.write(frame)
+
+        # Display the frame
+        # saved in the file
+        cv2.imshow('Frame', frame)
+
+        # Press S on keyboard
+        # to stop the process
+        t1 = time.time()  # current time
+        num_seconds = t1 - t0  # diff
+        if num_seconds > 10:
+            break
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            break
+
+    # Break the loop
+    else:
+        break
+# When everything done, release
+# the video capture and video
+# write objects
+video.release()
+result.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
+
+print("The video was successfully saved")
+
+
+# Preprocessing phase
+print("Reading + preprocessing video...")
+video_frames, frame_ct, fps = preprocessing.read_video('filename.mov')
+# Build Laplacian video pyramid
+print("Building Laplacian video pyramid...")
+lap_video = pyramids.build_video_pyramid(video_frames)
+
+amplified_video_pyramid = []
+
+for i, video in enumerate(lap_video):
+    if i == 0 or i == len(lap_video)-1:
+        continue
+
+    # Eulerian magnification with temporal FFT filtering
+    print("Running FFT and Eulerian magnification...")
+    result, fft, frequencies = eulerian.fft_filter(video, freq_min, freq_max, fps)
+    lap_video[i] += result
+
+    # Calculate heart rate
+    print("Calculating heart rate...")
+    heart_rate = heartrate.find_heart_rate(fft, frequencies, freq_min, freq_max)
+
+# Collapse laplacian pyramid to generate final video
+print("Rebuilding final video...")
+amplified_frames = pyramids.collapse_laplacian_video_pyramid(lap_video, frame_ct)
+
+# Output heart rate and final video
+print("Heart rate: ", heart_rate, "bpm")
+print("Displaying final video...")
+
+
